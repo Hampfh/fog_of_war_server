c98a066ee40b12028e7b829ad2fc3567ac1ca389
Hampus Hallkvist <hampus@hallkvist.org>
2021-05-06 19:14:49 +0200

    Fix socket group communication

--- src/logic.ts
+++ src/logic.ts
@@ -15,7 +15,7 @@ export default function onConnect(socket: Socket<DefaultEventsMap, DefaultEvents
 
 	console.log("New Connection! Total connections:", getConnectionCount())
 
-	socket.on("list_rooms", async () => {
+	socket.on("list_rooms", async (data) => {
 		const rawRooms = getRooms()
 		const rooms: { id: string, members: number }[] = []
 		for (const [id, room] of rawRooms.entries()) {
@@ -28,12 +28,15 @@ export default function onConnect(socket: Socket<DefaultEventsMap, DefaultEvents
 	socket.on("join_room", async roomCode => {
 		const success = joinRoom(socket.id, roomCode)
 		if (success) {
-			await socket.join(roomCode)
-			socket.emit("join_room_res", true)
-			return
+			try {
+				await socket.join(roomCode)
+				socket.emit("join_room_res", true)
+			} catch {
+				socket.emit("join_room_res", false)
+				return
+			}
 		}
-		socket.emit("join_room_res", false)
-		socket.to(roomCode).emit("opponent_connect")
+		socket.to(roomCode).emit("opponent_connect", "")
 	})
 
 	socket.on("create_room", async () => {
@@ -41,18 +44,22 @@ export default function onConnect(socket: Socket<DefaultEventsMap, DefaultEvents
 		if (roomCode.length <= 0)
 			socket.emit("create_room_res", false)
 		else {
-			await socket.join(roomCode)
-			socket.emit("create_room_res", roomCode)
+			try {
+				await socket.join(roomCode)
+				socket.emit("create_room_res", roomCode)
+			} catch {
+				socket.emit("create_room_res", false)
+			}
 		}
 	})
 
 	socket.on("opponent", data => {
 		const connection = getConnection(socket.id)
 		if (connection?.activeLobby) {
-			socket.to(connection.activeLobby).emit(data)
+			socket.to(connection.activeLobby).emit("opponent", data)
 		}
 		else
-			socket.emit("command_res", false)
+			socket.emit("opponent", false)
 	})
 
 	socket.on("disconnect", () => {
@@ -63,5 +70,9 @@ export default function onConnect(socket: Socket<DefaultEventsMap, DefaultEvents
 		// Remove connection and leave room
 		deleteConnection(socket.id)
 		console.log("Client left! Total connections:", getConnectionCount())
+	})
+
+	socket.on("error", error => {
+		console.warn(error)
 	})
 }
